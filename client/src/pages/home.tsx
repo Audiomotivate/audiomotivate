ed0f2888cef8738d5aea7488d5a908cb287be911
Audiomotivate <audiomotivateresumen@gmail.com>
2025-06-13 13:44:09 -0600

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,13 +1,13 @@
-import HeroSection from "@/components/hero-section";
-import AboutSection from "@/components/about-section";
-import AudiobooksSection from "@/components/audiobooks-section";
-import VideosSection from "@/components/videos-section";
-import GuidesSection from "@/components/guides-section";
-import PDFSection from "@/components/pdf-section";
-import TestimonialsSection from "@/components/testimonials-section";
-import ContactSection from "@/components/contact-section";
-import CallToAction from "@/components/call-to-action";
-import FeaturedQuote from "@/components/featured-quote";
+import HeroSection from "../components/hero-section";
+import AboutSection from "../components/about-section";
+import AudiobooksSection from "../components/audiobooks-section";
+import VideosSection from "../components/videos-section";
+import GuidesSection from "../components/guides-section";
+import PDFSection from "../components/pdf-section";
+import TestimonialsSection from "../components/testimonials-section";
+import ContactSection from "../components/contact-section";
+import CallToAction from "../components/call-to-action";
+import FeaturedQuote from "../components/featured-quote";
 
 export default function Home() {
   return (
