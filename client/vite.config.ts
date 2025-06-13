44f5b4c917840e6fafbf9118bb349b0093d97c60
Audiomotivate <audiomotivateresumen@gmail.com>
2025-06-13 12:30:34 -0600

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@shared': path.resolve(__dirname, '../shared'),
-      '@assets': path.resolve(__dirname, '../attached_assets'),
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+      "@assets": path.resolve(__dirname, "../attached_assets"),
     },
   },
+  root: ".",
   build: {
-    outDir: '../public',
-    emptyOutDir: true
-  }
-})
+    outDir: "../dist/public",
+    emptyOutDir: true,
+  },
+});
